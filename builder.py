4df4548498a1321da15eb24119049bfd21b79ae7
Steve0Greatness <steve0greatnessiscool@gmail.com>
2025-01-25 20:43:26 -0800

    Add temp removals
    Not having the webring on your site will result in a temp removal,
    rather than a full removal from now on.

--- builder.py
+++ builder.py
@@ -10,6 +10,10 @@ SITES: dict[str, dict] = {}
 with open("users.json") as file:
     SITES = load(file)
 
+TEMP_REMOVE_SITES: dict[str, dict] = {}
+with open("temprem.json") as file:
+    TEMP_REMOVE_SITES = load(file)
+
 # Below is XHTML because it is
 BringUserToSite = """
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -66,6 +70,48 @@ BringUserToSite = """
 
 </html>
 """
+TempRemNotification = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Temp. Removal: {{siteid}}</title>
+    <style type="text/css">
+    /*<![CDATA[*/
+    html {
+        background-color: #000;
+        color: #fff;
+    }
+    a {
+        color: #0af;
+        text-decoration: underline;
+    }
+    /*]]>*/
+    </style>
+</head>
+
+<body>
+    <h1>{{siteid}} has been temporarily removed from the webring</h1>
+    <p>
+        This site has temporarily been removed by the webring owner.
+        The given reason is:
+    </p>
+    <blockquote>{{reason}}</blockquote>
+    <p>
+        If you are {{siteid}}, please contact the webring
+        owner to have your site reinstated; otherwise, let {{siteid}}
+        know of this removal if possible.
+    </p>
+    <p>
+        <a href="{{siteurl}}">Here's a link back to {{siteid}} if
+        you need one</a>
+    </p>
+</body>
+
+</html>
+"""
+
 
 def main():
     try:
@@ -76,7 +122,7 @@ def main():
     mkdir(SITES_DIRECTORY)
 
     for index, cursite in enumerate(SITES):
-        print("Creating site directory for %s" % cursite["id"])
+        print("Creating site redirect for %s" % cursite["id"])
         maker = lambda site: BringUserToSite.replace("{{siteid}}", site["id"]).replace("{{siteurl}}", site["url"])
         NextIndex = index + 1 if index + 1 < len(SITES) else 0
         PrevIndex = index - 1 if index - 1 >= 0 else len(SITES) - 1
@@ -87,6 +133,14 @@ def main():
             file.write(Next)
         with open(joinpath(SITES_DIRECTORY, cursite["id"], "prev.%s" % USE_FILETYPE), "x") as file:
             file.write(Prev)
+    for index, cursite in enumerate(TEMP_REMOVE_SITES):
+        print("Creating notice for %s" % cursite["id"])
+        site = TempRemNotification.replace("{{siteid}}", cursite["id"]).replace("{{reason}}", cursite["reason"]).replace("{{siteurl}}", cursite["url"])
+        mkdir(joinpath(SITES_DIRECTORY, cursite["id"]))
+        with open(joinpath(SITES_DIRECTORY, cursite["id"], "next.%s" % USE_FILETYPE), "x") as file:
+            file.write(site)
+        with open(joinpath(SITES_DIRECTORY, cursite["id"], "prev.%s" % USE_FILETYPE), "x") as file:
+            file.write(site)
     print("Finished making sites directory")
 
 if __name__ == "__main__":
